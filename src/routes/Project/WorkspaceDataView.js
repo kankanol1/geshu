199b760defdcdc20b1f0086afe264260e953936a
Yestin <873915231@qq.com>
2018-02-13 21:45:31 -0500

    minor change.

--- src/routes/Project/WorkspaceDataView.js
+++ src/routes/Project/WorkspaceDataView.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
+import { Layout, Card } from 'antd';
 import WorkSpaceMenu from './WorkspaceMenu';
+import WorkAreaMenu from './Workspace/WorkAreaMenu';
+
 const { Header } = Layout;
 
 export default class WorkspaceDataView extends Component {
@@ -8,10 +10,16 @@ export default class WorkspaceDataView extends Component {
     return (
       <Layout style={{ padding: '0', height: '100%' }} theme="light" >
         <Header style={{ padding: '0px', height: '48px', lineHeight: '46px', background: '#eee' }}>
+          <WorkAreaMenu />
           <WorkSpaceMenu currentPath={this.props.location} />
         </Header>
         <Layout style={{ padding: '0', height: '100%' }} theme="light">
-          <div>Hello data</div>
+          <Card>
+            <div>Hello data</div>
+          </Card>
+          <Card>
+            <div>Here goes the data display</div>
+          </Card>
         </Layout>
       </Layout>
     );
--- src/routes/Project/WorkspaceLogView.js
+++ src/routes/Project/WorkspaceLogView.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
+import { Layout, Card } from 'antd';
 import WorkSpaceMenu from './WorkspaceMenu';
+import WorkAreaMenu from './Workspace/WorkAreaMenu';
 
 const { Header } = Layout;
 
-export default class WorkspaceEditor extends Component {
+export default class WorkspaceLogView extends Component {
   render() {
     return (
       <Layout style={{ padding: '0' }} theme="light" >
         <Header style={{ padding: '0px', height: '48px', lineHeight: '46px', background: '#eee' }}>
+          <WorkAreaMenu />
           <WorkSpaceMenu currentPath={this.props.location} />
         </Header>
         <Layout style={{ padding: '0', height: '100%' }} theme="light">
-          <div>Hello log</div>
+          <Card>
+            <div>Hello log</div>
+          </Card>
+          <Card>
+            <div>Here goes the log display</div>
+          </Card>
         </Layout>
       </Layout>
     );
