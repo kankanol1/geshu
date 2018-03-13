a0b773db6900b4d7a006fe47f88fd423004f7806
Yueting Chen <873915231@qq.com>
2018-03-13 16:58:35 -0400

    remove unnecessary stuff in login page.

--- src/routes/User/Login.js
+++ src/routes/User/Login.js
@@ -13,12 +13,7 @@ const { Tab, UserName, Password, Mobile, Captcha, Submit } = Login;
 }))
 export default class LoginPage extends Component {
   state = {
-    type: 'account',
     autoLogin: true,
-  }
-
-  onTabChange = (type) => {
-    this.setState({ type });
   }
 
   handleSubmit = (err, values) => {
@@ -56,38 +51,21 @@ export default class LoginPage extends Component {
           onTabChange={this.onTabChange}
           onSubmit={this.handleSubmit}
         >
-          <Tab key="account" tab="账户密码登录">
-            {
+          {
               login.status === 'error' &&
-              login.type === 'account' &&
               !login.submitting &&
               this.renderMessage('账户或密码错误（admin/888888）')
             }
-            <UserName name="userName" placeholder="admin/user" />
-            <Password name="password" placeholder="888888/123456" />
-          </Tab>
-          <Tab key="mobile" tab="手机号登录">
-            {
-              login.status === 'error' &&
-              login.type === 'mobile' &&
-              !login.submitting &&
-              this.renderMessage('验证码错误')
-            }
-            <Mobile name="mobile" />
-            <Captcha name="captcha" />
-          </Tab>
+          <UserName name="userName" placeholder="admin/user" />
+          <Password name="password" placeholder="888888/123456" />
           <div>
             <Checkbox checked={this.state.autoLogin} onChange={this.changeAutoLogin}>自动登录</Checkbox>
             <a style={{ float: 'right' }} href="">忘记密码</a>
           </div>
           <Submit loading={submitting}>登录</Submit>
-          <div className={styles.other}>
-            其他登录方式
-            <Icon className={styles.icon} type="alipay-circle" />
-            <Icon className={styles.icon} type="taobao-circle" />
-            <Icon className={styles.icon} type="weibo-circle" />
+          {/* <div className={styles.other}>
             <Link className={styles.register} to="/user/register">注册账户</Link>
-          </div>
+          </div> */}
         </Login>
       </div>
     );
