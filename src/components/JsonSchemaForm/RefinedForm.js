1af177a8d8dc4a6ba1a258a30e80ca283d2f7f86
Yueting Chen <873915231@qq.com>
2018-08-31 10:53:23 -0400

    support save in case of form validation error.

--- src/components/JsonSchemaForm/RefinedForm.js
+++ src/components/JsonSchemaForm/RefinedForm.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import Form from 'react-jsonschema-form';
-import { retrieveSchema } from 'react-jsonschema-form/lib/utils';
+import { retrieveSchema, setState } from 'react-jsonschema-form/lib/utils';
 import validateFormData, { toErrorList } from './validate';
 
 export default class RefinedForm extends Form {
+  componentWillMount() {
+    const { errors, errorSchema } = this.validate(this.state.formData);
+    this.setState({ ...this.state, errors, errorSchema, submitted: false });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const stat = this.getStateFromProps(nextProps);
+    if (this.state.submitted) {
+      // only validate after a submittion.
+      const { errors, errorSchema } = this.validate(stat.formData);
+      this.setState({ ...stat, errors, errorSchema, submitted: false });
+    } else {
+      this.setState(stat);
+    }
+  }
+
   validate(formData, schema = this.props.schema) {
     const { validate, transformErrors } = this.props;
     const { definitions } = this.getRegistry();
@@ -15,4 +31,34 @@ export default class RefinedForm extends Form {
       transformErrors
     );
   }
+
+  // override submit
+  onSubmit = (event) => {
+    event.preventDefault();
+
+    if (!this.props.noValidate) {
+      const { errors, errorSchema } = this.validate(this.state.formData);
+      if (Object.keys(errors).length > 0) {
+        setState(this, { errors, errorSchema }, () => {
+          if (this.props.onError) {
+            this.props.onError(errors);
+          } else {
+            // eslint-disable-next-line
+            console.error('Form validation failed', errors);
+          }
+
+          if (this.props.onSubmit) {
+            this.props.onSubmit({ ...this.state, status: 'submitted', submitted: true });
+          }
+        });
+        return;
+      }
+    }
+
+    setState(this, { errors: [], errorSchema: {} }, () => {
+      if (this.props.onSubmit) {
+        this.props.onSubmit({ ...this.state, status: 'submitted', submitted: true });
+      }
+    });
+  };
 }
