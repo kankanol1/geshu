2b0e4021acef61027a907c001322c858e76c867e
Yueting Chen <873915231@qq.com>
2018-09-28 15:40:17 -0400

    fix build script behavior on linux.

--- scripts/build.js
+++ scripts/build.js
@@ -4,10 +4,11 @@
 
 const shell = require('shelljs');
 
-const buildOpt = process.argv.length === 2 ? '*' : process.argv[2];
+const partOpt = process.argv.filter((_, i) => i > 0 && process.argv[i-1] === '-part');
+const buildOpt = partOpt.length === 0 ? '-' : partOpt[0];
 
-const devOptInput = process.argv.length === 3 ? 'release': process.argv[3];
-const isRelease = devOptInput === 'dev' ? false : true;
+const isRelease = !process.argv.includes('-dev');
+console.log('buildOpt', buildOpt);
 
 const tmpDir = 'dist-tmp';
 
@@ -16,7 +17,7 @@ if (shell.test('-d', tmpDir)) {
 }
 shell.mkdir(tmpDir);
 
-if (buildOpt === '*' || buildOpt === 'all') {
+if (buildOpt === '-' || buildOpt === 'all') {
   // build all.
   shell.exec('npm run setup');
   shell.exec(`npm run ${isRelease ? 'build' : 'dev-build'}`);
@@ -25,7 +26,7 @@ if (buildOpt === '*' || buildOpt === 'all') {
   shell.cp('-R', 'dist/*', 'dist-tmp/all/');
 }
 
-if (buildOpt === '*' || buildOpt === 'ml') {
+if (buildOpt === '-' || buildOpt === 'ml') {
   // build ml part.
   shell.exec('npm run setup:ml');
   shell.exec(`npm run ${isRelease ? 'build' : 'dev-build'}`);
@@ -34,7 +35,7 @@ if (buildOpt === '*' || buildOpt === 'ml') {
   shell.cp('-R', 'dist/*', 'dist-tmp/ml/');
 }
 
-if (buildOpt === '*' || buildOpt === 'graph') {
+if (buildOpt === '-' || buildOpt === 'graph') {
   // build graph part.
   shell.exec('npm run setup:graph');
   shell.exec(`npm run ${isRelease ? 'build' : 'dev-build'}`);
